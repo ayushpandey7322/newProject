36c90f1f018ebafed11cea02b86446e2ffb70980
root <root@ace123.novalocal>
2022-04-14 09:51:54 +0000

    Cors issue

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const bodyparser = require('body-parser');
 app.use(bodyparser.urlencoded({ extended: false }))                                                                                                                             // frontend ke data ko receive in json form and then save
 app.use(bodyparser.json());
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent// to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 
 const  postRoutes  = require('./api/routers/postRoutes');
 const userRoutes = require('./api/routers/userRoutes');
